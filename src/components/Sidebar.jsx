4357d9f740f4cab263218d01f00e21bfec275f0f
amruta-dhage <amrutadhage97@gmail.com>
2024-08-06 17:30:07 +0530

    fixed one issue

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,28 +6,28 @@ import { BiBookBookmark } from "react-icons/bi";
 import { IoMdLogOut } from "react-icons/io";
 function Sidebar() {
   return (
-    <div className="max-w-16 container mx-auto  bg-gray-800  fixed h-full z-50 rounded-l-lg">
-      <ul className="mt-5 space-y-6">
+    <div className="max-w-16 container mx-auto bg-gray-800 fixed h-full z-50 rounded-l-lg flex flex-col">
+      <ul className="mt-5 space-y-6 flex-grow">
         <li className="text-white flex justify-center">
           <TfiMenuAlt size={24} color={"#428ced"} />
         </li>
-        <li className="text-white flex justify-center border-2  border-gray-800 border-l-indigo-500 ">
+        <li className="text-white flex justify-center border-2 border-gray-800 border-l-indigo-500">
           <AiFillHome size={24} color={"#428ced"} />
         </li>
-        <li className="text-white flex justify-center  border-top-2px-blue ">
+        <li className="text-white flex justify-center border-top-2px-blue">
           <BsFileBarGraph size={24} />
         </li>
-        <li className="text-white flex justify-center  border-top-2px-blue ">
+        <li className="text-white flex justify-center border-top-2px-blue">
           <BsCalendar2Check size={20} />
         </li>
-        <li className="text-white flex justify-center  border-top-2px-blue ">
+        <li className="text-white flex justify-center border-top-2px-blue">
           <BiBookBookmark size={24} />
         </li>
-        <li className="text-white flex justify-center  border-top-2px-blue ">
+        <li className="text-white flex justify-center border-top-2px-blue">
           <BsCalendar2Check size={20} />
         </li>
       </ul>
-      <div className="text-white flex justify-center items-end my-64 md:my-96">
+      <div className="text-white flex justify-center mb-4">
         <IoMdLogOut size={24} />
       </div>
     </div>
